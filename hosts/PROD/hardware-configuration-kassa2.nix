a27b2fcc226e7984ba33dcd38d343baff50858be
Thomas <thomas.erents@gmail.com>
2026-07-15 21:43:55 +0200

    Feat: Add configuration to make `nix flake check` shut up

--- hosts/PROD/hardware-configuration-kassa2.nix
+++ hosts/PROD/hardware-configuration-kassa2.nix
@@ -1,3 +1,12 @@
-{
+{ lib, ... }:
 
+{
+  fileSystems."/" = {
+    device = "/nonexistent";
+    fsType = "ext4";
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  boot.loader.grub.device = "nodev";
+  system.stateVersion = "26.05";
 }
