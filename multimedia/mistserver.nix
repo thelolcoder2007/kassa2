c310cdbeaf8e3ee945a6dfe0f03d2fa861c6141c
Thomas <thomas.erents@gmail.com>
2026-07-16 16:05:15 +0200

    `nix fmt`

--- multimedia/mistserver.nix
+++ multimedia/mistserver.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ../pkgs/mistserver/options.nix
@@ -25,13 +30,15 @@
     description = "Stream /dev/video0 to RTMP";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      Environment="GST_PLUGIN_SYSTEM_PATH_1_0=\"${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0\"";
+      Environment = "GST_PLUGIN_SYSTEM_PATH_1_0=\"${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0\"";
       Type = "simple";
       Restart = "always";
       RestartSec = 2;
       TasksMax = "infinity";
       TimeoutStopSec = 8;
-      ExecStart = "${lib.getExe pkgs.bash} -c \"${pkgs.gst_all_1.gstreamer}/bin/gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! x264enc ! flvmux ! rtmpsink location=rtmp://127.0.0.1/live/$(${pkgs.coreutils-full}/bin/cat ${config.sops.secrets."rtmp_key".path})\"";
+      ExecStart = "${lib.getExe pkgs.bash} -c \"${pkgs.gst_all_1.gstreamer}/bin/gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! x264enc ! flvmux ! rtmpsink location=rtmp://127.0.0.1/live/$(${pkgs.coreutils-full}/bin/cat ${
+        config.sops.secrets."rtmp_key".path
+      })\"";
       User = "root";
       Group = "root";
 
