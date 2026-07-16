35fb543c777d9110d4147976024ce798e5649b46
Thomas <thomas.erents@gmail.com>
2026-07-16 20:48:25 +0200

    Chore: Move gstreamer things into gstreamer.nix, instead of in mistserver.nix

--- multimedia/gstreamer.nix
+++ multimedia/gstreamer.nix
@@ -1,19 +1,62 @@
-{ pkgs, ... }:
-
 {
-  environment.systemPackages =
-    with pkgs.gst_all_1;
-    [
-      # keep-sorted start
-      gst-libav
-      gst-plugins-bad
-      gst-plugins-base
-      gst-plugins-good
-      gst-plugins-ugly
-      gstreamer
-      # keep-sorted end
-    ]
-    ++ [
-      pkgs.v4l2-relayd
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  gstreamer-sh = pkgs.writeShellScriptBin "gstreamer.sh" ''
+    ${pkgs.gst_all_1.gstreamer}/bin/gst-launch-1.0 \
+        v4l2src device=/dev/video0 ! videoconvert ! "video/x-raw, width=3840, height=2160, framerate=60/1" \
+        ! queue ! x264enc cabac=1 bframes=2 ref=1 ! "video/x-h264,profile=main" ! flvmux streamable=true name=muxer \
+        ! rtmpsink location="rtmp://127.0.0.1/live/$(${pkgs.coreutils-full}/bin/cat ${
+          config.sops.secrets."rtmp_key".path
+        })"
+  '';
+in
+{
+  imports = [
+    ../base/sops.nix
+  ];
+  environment.systemPackages = config.systemd.services."gstreamer-stream".path;
+
+  sops.secrets.rtmp_key = {
+    restartUnits = [ "gstreamer-stream.service" ];
+  };
+
+  systemd.services."gstreamer-stream" = {
+    after = [
+      "network.target"
+      "mistserver.service"
     ];
+    description = "Stream /dev/video0 to RTMP";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Environment = "GST_PLUGIN_SYSTEM_PATH_1_0=\"${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0\"";
+      Type = "simple";
+      Restart = "always";
+      RestartSec = 2;
+      TasksMax = "infinity";
+      TimeoutStopSec = 8;
+      ExecStart = "${lib.getExe pkgs.bash} ${lib.getExe gstreamer-sh}";
+      User = "root";
+      Group = "root";
+
+    };
+    path =
+      with pkgs.gst_all_1;
+      [
+        # keep-sorted start
+        gst-libav
+        gst-plugins-bad
+        gst-plugins-base
+        gst-plugins-good
+        gst-plugins-ugly
+        gstreamer
+        # keep-sorted end
+      ]
+      ++ [
+        pkgs.v4l2-relayd
+      ];
+  };
 }
--- multimedia/mistserver.nix
+++ multimedia/mistserver.nix
@@ -1,18 +1,13 @@
 {
   config,
-  lib,
-  pkgs,
   ...
 }:
 {
-  environment.systemPackages = [ config.services.mistServer.package ];
   imports = [
     ../pkgs/mistserver/options.nix
-    ../base/sops.nix
   ];
-  sops.secrets.rtmp_key = {
-    restartUnits = [ "gstreamer-stream.service" ];
-  };
+
+  environment.systemPackages = [ config.services.mistServer.package ];
 
   networking.firewall.extraInputRules = ''
     ip6 saddr { 2a07:54c1:4932::/48, 2001:67c:2564::/48 } tcp dport 4242 accept
@@ -23,42 +18,5 @@
     enable = true;
     openFirewall = true;
   };
-  systemd.services."gstreamer-stream" = {
-    after = [
-      "network.target"
-      "mistserver.service"
-    ];
-    description = "Stream /dev/video0 to RTMP";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Environment = "GST_PLUGIN_SYSTEM_PATH_1_0=\"${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0\"";
-      Type = "simple";
-      Restart = "always";
-      RestartSec = 2;
-      TasksMax = "infinity";
-      TimeoutStopSec = 8;
-      ExecStart = "${lib.getExe pkgs.bash} -c \"${pkgs.gst_all_1.gstreamer}/bin/gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! x264enc ! flvmux ! rtmpsink location=rtmp://127.0.0.1/live/$(${pkgs.coreutils-full}/bin/cat ${
-        config.sops.secrets."rtmp_key".path
-      })\"";
-      User = "root";
-      Group = "root";
-
-    };
-    path =
-      with pkgs.gst_all_1;
-      [
-        # keep-sorted start
-        gst-libav
-        gst-plugins-bad
-        gst-plugins-base
-        gst-plugins-good
-        gst-plugins-ugly
-        gstreamer
-        # keep-sorted end
-      ]
-      ++ [
-        pkgs.v4l2-relayd
-      ];
-  };
-  networking.firewall.allowedTCPPorts = [80];
+  networking.firewall.allowedTCPPorts = [ 80 ];
 }
