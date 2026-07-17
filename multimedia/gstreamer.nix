5ee89a09e4859837c8224d2734dfd79dbea892fb
Thomas <thomas.erents@gmail.com>
2026-07-17 22:26:44 +0200

    Fix: Have another gstreamer command, it will fix everything, trust

--- multimedia/gstreamer.nix
+++ multimedia/gstreamer.nix
@@ -5,13 +5,18 @@
   ...
 }:
 let
-  gstreamer-sh = pkgs.writeShellScriptBin "gstreamer.sh" ''
+  gstreamer-sh = pkgs.writeShellScript "gstreamer.sh" ''
+    sleep 5
     ${pkgs.gst_all_1.gstreamer}/bin/gst-launch-1.0 -v v4l2src device=/dev/video0 \
-    ! videoconvert ! x264enc quantizer=0 speed-preset=ultrafast tune=zerolatency \
-    ! h264parse ! mpegtsmux \
-    ! srtsink uri="srt://127.0.0.1:8889?streamid=$(${pkgs.coreutils-full}/bin/cat ${
-      config.sops.secrets."rtmp_key".path
-    })"
+        ! video/x-raw,format=Y444,width=3840,height=2160,frames=60/1 ! tee name=t \
+        t. ! queue ! x264enc quantizer=0 speed-preset=ultrafast \
+        tune=zerolatency ! h264parse \
+        ! srtsink uri="srt://127.0.0.1:8889?streamid=$(${pkgs.coreutils-full}/bin/cat ${
+          config.sops.secrets."rtmp_key".path
+        })" \
+        t. ! queue ! avenc_ffv1 ! matroskamux ! filesink location=/var/mistserver/recording/sntpings-$(date +%Y-%m-%d_%H-%M-%S).mkv \
+        t. ! queue ! videorate ! video/x-raw,framerate=1/1 ! videoconvert ! pngenc ! multifilesink location=sntpings-$(date +%Y-%m-%d_%H-%M-%S)-frame%06d.png
+
   '';
 in
 {
@@ -38,7 +43,7 @@ in
       RestartSec = 2;
       TasksMax = "infinity";
       TimeoutStopSec = 8;
-      ExecStart = "${lib.getExe pkgs.bash} ${lib.getExe gstreamer-sh}";
+      ExecStart = "${lib.getExe pkgs.bash} ${gstreamer-sh}";
       User = "root";
       Group = "root";
 
