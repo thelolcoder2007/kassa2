a5d1314a889e7843c0f534e27766b294f4bd7cc3
Thomas <thomas.erents@gmail.com>
2026-09-02 14:10:29 +0200

    ffmpeg: Fix streaming branch

--- multimedia/ffmpeg.nix
+++ multimedia/ffmpeg.nix
@@ -1,16 +1,11 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   ffmpeg-sh = pkgs.writeShellScript "ffmpeg.sh" ''
-    rtmp_key=$(${pkgs.coreutils-full}/bin/cat ${config.sops.secrets."rtmp_key".path})
+    rtmp_key=$(${lib.getExe' pkgs.coreutils-full "cat"} ${config.sops.secrets."rtmp_key".path})
 
-    # The first branch might be a bad idea, since this is quite a lot of data.
-    # Let's hope that Mistserver compresses it into tiny little pieces so end devices don't consume 100mbit/s just viewing this livestream (it did...)
-    ffmpeg -f v4l2 -video_size 3840x2160 -framerate 60 \
-    -thread_queue_size 1024 -i /dev/video0 \
-    -map 0:v -c:v libx265 -g 120 -b:v 100M -maxrate 100M -bufsize 200M -f rtsp "rtsp://127.0.0.1:5554/$rtmp_key" \
-    -map 0:v -vf fps=1 -f image2 -y -strftime 1 "/run/mistserver/%S.png" \
-    -map 0:v -c:v libx264 -preset ultrafast -qp 0 -threads 0 -f matroska "/run/mistserver-recordings/sntpings-$(date +%Y-%m-%d_%H-%M-%S).mkv"
+    ffmpeg -f v4l2 -video_size 3840x2160 -framerate 60  -i /dev/video0 \
+    -map 0:v -c:v libx264 -crf 20 -preset ultrafast -g 30 -threads 1 -f rtsp "rtsp://127.0.0.1:5554/$rtmp_key"
   '';
 in
 {
